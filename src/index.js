113a711ee52e8d69ba951fd05219a1d5a03dff2c
Mritunjay-Choudhary <mritunjayc517@gmail.com>
2026-09-26 17:36:47 +0530

    next commit

--- src/index.js
+++ src/index.js
@@ -5,4 +5,15 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is Listening on port ${process.env.PORT}`);
+        
+    })
+
+})
+.catch((error) =>{
+    console.log("Mongo DB Connection Failed", error);
+    
+})
